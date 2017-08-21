73b5390484e7887204504b8a9d74f88df517ff69
LucaJRossi <lucarossi@swin.edu.au>
2017-08-21 14:21:18 +1000

    Add files via upload

---
+++ MW_GAL/SC6/Plot_orbit.m
@@ -0,0 +1,58 @@
+clear all
+close all
+
+data = load('ax_coeff.dat');
+
+whitebg('w')
+
+t = data(:,1);
+
+x = data(:,12);
+y = data(:,13);
+z = data(:,14);
+
+v_x = data(:,15);
+v_y = data(:,16);
+v_z = data(:,17);
+
+R = sqrt(x.^2+y.^2);
+r = sqrt(x.^2+y.^2+z.^2);
+
+
+
+subplot(2,2,1), plot(x,y,'k')
+
+xlabel('x (kpc)','fontsize',9)
+ylabel('y (kpc)','fontsize',9)
+axis([-8 8 -8 8])
+set(gca,'xtick',(-8:2:8),'ytick',(-8:2:8),'fontsize',8)
+axis square
+
+
+subplot(2,2,2), plot(x,z,'k')
+
+xlabel('x (kpc)','fontsize',9)
+ylabel('z (kpc)','fontsize',9)
+axis([-8 8 -8 8])
+set(gca,'xtick',(-8:2:8),'ytick',(-8:2:8),'fontsize',8)
+axis square
+
+subplot(2,2,3), plot(t,r,'k')
+
+xlabel('t (Myr)','fontsize',9)
+ylabel('r (kpc)','fontsize',9)
+axis([0 max(t) 0 8])
+set(gca,'ytick',(0:2:8),'fontsize',8)
+axis square
+
+
+subplot(2,2,4), plot(R,z,'k')
+
+xlabel('R (kpc)','fontsize',9)
+ylabel('z (kpc)','fontsize',9)
+axis square
+axis([0 8 -8 8])
+set(gca,'fontsize',8)
+
+
+
